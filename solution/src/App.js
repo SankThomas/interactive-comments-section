d9b4816f40ec6fc0dfbd901877b9cbb51320f33d
Thomas Sankara <thomassankara74@gmail.com>
2022-01-05 09:44:01 +0300

    render Comments component

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,5 +1,13 @@
+import Comments from "./components/Comments"
+
 function App() {
-  return <div></div>
+  return (
+    <>
+      <main className="p-5 md:max-w-3xl md:mx-auto lg:py-10">
+        <Comments />
+      </main>
+    </>
+  )
 }
 
 export default App
